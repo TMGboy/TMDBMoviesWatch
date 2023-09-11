8f72f614fbb0dd9c97c28654ec23389e621264c6
TMGBoy <140494682+TMGboy@users.noreply.github.com>
2023-09-11 03:14:17 +0100

    Update updates.js

--- updates.js
+++ updates.js
@@ -1,50 +1,13 @@
 // ==UserScript==
 // @name         TMDb Movies
 // @namespace    https://cima4u.org/
-// @version      2.0
+// @version      1.0
 // @description  Play Any Movies/TV From TMDB
 // @author       TMGBoy
 // @match        https://www.themoviedb.org/movie/*
 // @match        https://www.themoviedb.org/tv/*
-// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/TMGboy/TMDBMoviesWatch/main/updates.js
+// @updateURL    https://raw.githubusercontent.com/TMGboy/TMDBMoviesWatch/main/updates.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    const buttonImageUrl = 'https://cima4u.org/favicon/icon512.png';
-
-    function createWatchButton() {
-        const button = document.createElement('a');
-        button.href = getRedirectUrl();
-        button.target = '_self';
-        button.style.position = 'fixed';
-        button.style.right = '60px'; 
-        button.style.top = '50%';
-        button.style.transform = 'translateY(-50%)';
-        button.style.zIndex = '9999';
-
-        const img = document.createElement('img');
-        img.src = buttonImageUrl;
-        img.alt = 'Watch Now';
-        img.style.width = '92px';
-
-        button.appendChild(img);
-
-        document.body.appendChild(button);
-    }
-
-    function getRedirectUrl() {
-        const currentUrl = window.location.href;
-        if (currentUrl.includes('/movie/')) {
-            return currentUrl.replace('https://www.themoviedb.org/movie/', 'https://cima4u.org/movie/');
-        } else if (currentUrl.includes('/tv/')) {
-            return currentUrl.replace('https://www.themoviedb.org/tv/', 'https://www.cima4u.org/tv/');
-        }
-        return '';
-    }
-
-    if (window.location.href.includes('https://www.themoviedb.org/')) {
-        createWatchButton();
-    }
-})();
+!function(){const e="https://cima4u.org/favicon/icon512.png";function t(){const t=document.createElement("a");t.href=n(),t.target="_self",t.style.position="fixed",t.style.right="60px",t.style.top="50%",t.style.transform="translateY(-50%)",t.style.zIndex="9999";const o=document.createElement("img");o.src=e,o.alt="Watch Now",o.style.width="92px",t.appendChild(o),document.body.appendChild(t)}function n(){const e=window.location.href;return e.includes("/movie/")?e.replace("https://www.themoviedb.org/movie/","https://cima4u.org/movie/"):e.includes("/tv/")?e.replace("https://www.themoviedb.org/tv/","https://www.cima4u.org/tv/"):""}window.location.href.includes("https://www.themoviedb.org/")&&t()}();
